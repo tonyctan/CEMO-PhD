56fc6264e5c3a4fa8ac9eaf3e04f69ef2f09138a
Tony Tan <tony.c.tan@gmail.com>
2022-04-15 14:07:18 +0200

    Add descriptive statistics to all registry data sets

--- Data/Audit/Variable_names.R
+++ Data/Audit/Variable_names.R
@@ -12,8 +12,8 @@
 # Computer environment (store-view-edit-execute): any-any-any-TSD
 
 ##### Begin script #####
- ###                ###
-  #                  #
+ ###                ### 
+  #                  #  
 
 # Point working directory to the location of all registry datasets
 setwd("N:/durable/data/registers/")
@@ -24,32 +24,65 @@ file_names <- list.files(getwd(), pattern = "*.csv", full.names = F)
 # Create a placeholder matrix to receive dimension info of each dataset
 dim_list <- matrix(nrow = length(file_names), ncol = 2)
 
-# Read in each data file
-data.table::setDTthreads(16) # Use all 16 CPU cores
+# Create a placeholder matrix to receive number of NA info
 
-# Save variable names of each dataset into a separate file
+# Read in each data file
+data.table::setDTthreads(16) # Use all 16 CPU cores---not interesting in Windows but will be much more efficient in Linux
+
+# Save descriptive statistics of each variable
 for (i in 1:length(file_names)) {
     temp <- data.table::fread(file_names[i]) # Read in dataset one at a time
 
     dim_list[i, ] <- dim(temp) # Save dimension info to dim_list
 
-    var_names <- attributes(temp)[1] # Extract variable names
+    # Variable name
+    var_names <- data.frame(attributes(temp)[1])[,1] # Extract variable names
 
-    write.table(var_names, # Save variable names to M drive
+    # Missings
+    na_count <- data.frame(colSums(is.na(temp)))[,1] # Number of missings
+    na_percent <- na_count / dim(temp)[1] * 100 # Turn to missing percentages
+
+    # Descriptive statistics
+    # parallel::mclapply() not interesting for Windows but super quick for Linux
+    min <- as.numeric(parallel::mclapply(temp, function(x) min(x, na.rm=T)))
+    max <- as.numeric(parallel::mclapply(temp, function(x) max(x, na.rm=T)))
+    mean <- as.numeric(parallel::mclapply(temp, function(x) mean(x, na.rm=T)))
+    median <- as.numeric(parallel::mclapply(temp, function(x) median(x, na.rm=T)))
+    variance <- as.numeric(parallel::mclapply(temp, function(x) var(x, na.rm=T)))
+    sd <- as.numeric(parallel::mclapply(temp, function(x) sd(x, na.rm=T)))
+    # Skewness and kurtosis need extra packages
+    skewness <- as.numeric(parallel::mclapply(temp, function(x) DescTools::Skew(x, na.rm=T)))
+    kurtosis <- as.numeric(parallel::mclapply(temp, function(x) DescTools::Kurt(x, na.rm=T)))
+
+    write.table(
+        cbind(var_names, # This column is text.
+            round(cbind(
+                na_count, na_percent,
+                min, max,
+                mean, median,
+                variance, sd,
+                skewness, kurtosis),
+            digits = 2) # These columns are numeric
+        ), # Text + numeric = quotation marks everywhere
         paste0("M:/p1708-tctan/Documents/Variable_names/", file_names[i]),
-        quote = F # Do not put quotation marks around data
+        quote = F, # Remove all quotation marks
+        col.names = F # Do not print column names
     )
 
-    rm(temp) # Dump the temporary file to free up RAM
-    rm(var_names) # Dump the temporary variable name list for next cycle
+    # Dump temporary files and variables to free up RAM
+    rm(temp)
+    rm(var_names)
+    rm(c(na_count, na_percent))
+    rm(c(min, max, mean, median, variance, sd, skewness, kurtosis))
 }
 
 # Save dimension info of all datasets
 write.table(cbind(file_names, dim_list),
     "M:/p1708-tctan/Documents/Variable_names/dim_list.csv",
-    quote = F # Do not put quotation marks around data
+    quote = F, # Do not put quotation marks around data
+    col.names = F # Do not print column names
 )
 
-  #                #
- ###              ###
+  #                #  
+ ###              ### 
 ##### End script #####

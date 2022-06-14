2d838260943635b0d3e28a1006561cf2fcfde183
Tony Tan <tony.c.tan@gmail.com>
2022-06-14 19:22:29 +0200

    Add Udemy Python course material

---
+++ Teaching/Python/Colt/Exercises/magic_trick_starter.py
@@ -0,0 +1,186 @@
+#   __  __             _      
+#  |  \/  |           (_)     
+#  | \  / | __ _  __ _ _  ___ 
+#  | |\/| |/ _` |/ _` | |/ __|
+#  | |  | | (_| | (_| | | (__ 
+#  |_|  |_|\__,_|\__, |_|\___|
+#                 __/ |       
+#                |___/        
+
+# Create a variable called secret_num and set it to any number you'd like:
+
+
+# Increase secret_num by 1
+
+
+# Next, take the new number and double it
+
+
+# Next, take the new number and add four to it
+
+
+# Next, take the new number and divide by two
+
+
+# Next, take the new number and subtract your original secret number from it
+
+
+# Print the final result
+
+
+
+# To see my magical prediction, scroll down...
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+# ↓
+
+#   ____    
+#  |___ \   
+#    __) |  
+#   |__ <   
+#   ___) |  
+#  |____/   
+          
+# DID YOU SEE 3 PRINTED OUT???

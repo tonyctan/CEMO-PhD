29b2fe0b55f954a67cb86cb3ee6eb9eec2dc00c5
Tony Tan <tony.c.tan@gmail.com>
2022-06-17 18:41:29 +0200

    Practice Python using magic_trick_starter.py

--- Teaching/Python/Colt/Exercises/magic_trick_starter.py
+++ Teaching/Python/Colt/Exercises/magic_trick_starter.py
@@ -8,25 +8,25 @@
 #                |___/        
 
 # Create a variable called secret_num and set it to any number you'd like:
-
+secret_num = 26
 
 # Increase secret_num by 1
-
+secret_num += 1
 
 # Next, take the new number and double it
-
+secret_num *= 2
 
 # Next, take the new number and add four to it
-
+secret_num += 4
 
 # Next, take the new number and divide by two
-
+secret_num /= 2
 
 # Next, take the new number and subtract your original secret number from it
-
+secret_num -= 26
 
 # Print the final result
-
+print(secret_num)
 
 
 # To see my magical prediction, scroll down...

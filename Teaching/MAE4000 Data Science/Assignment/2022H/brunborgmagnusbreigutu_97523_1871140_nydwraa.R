f1782bdce5b0e2ebcdd8fc2c3646f752dbd7342c
Tony Tan <tony.c.tan@gmail.com>
2022-09-15 20:56:27 +0200

    Mark Magnus's MAE4000 assignment

--- Teaching/MAE4000 Data Science/Assignment/2022H/brunborgmagnusbreigutu_97523_1871140_nydwraa.R
+++ Teaching/MAE4000 Data Science/Assignment/2022H/brunborgmagnusbreigutu_97523_1871140_nydwraa.R
@@ -7,15 +7,14 @@ library(tidyverse)
 
 #####import Ydata and RTdata
 ####the imported Ydata is a tibble, making it a data.frame:
-
-RTdataG= read.table(file = "../Data/RTdata.csv",header = TRUE,sep = ";",dec = ",")
-Ydata<-read_excel("../Data/Ydata.xlsx")
-
+RTdata= read.table(file = "data/RTdata.csv",header = TRUE,sep = ";",dec = ",")
+Ydata<-read_excel("data/Ydata.xlsx")
 Ydata1=as.data.frame(Ydata)
 
 RTdata1=RTdata[,c("ID","Time01","Time02","Time03","Time04","Time05","Time06","Time07","Time08","Time09",
                   "Time10","Time11","Time12","Time13","Time14","Time15","Time16","Time17","Time18","Time19","Time20","Time21")]
 
+RTdata2<-RTdata1 %>%distinct(ID,.keep_all = TRUE)
 LongY=reshape(Ydata1,direction = "long",idvar = "ID","Y","Item",
               varying = list(c("Item_1","Item_2","Item_3","Item_4","Item_5","Item_6","Item_7","Item_8","Item_9",
                                "It_10","It_11","It_12","It_13","It_14","It_15","It_16","It_17","It_18","It_19","It_20","It_21")))
@@ -26,7 +25,9 @@ LongRT=reshape(RTdata2,direction = "long",idvar =  "ID","RT","Item",
 
 RTdata2<-RTdata1 %>%distinct(ID,.keep_all = TRUE)
 
-RTdataG1<-RTdataG %>% distinct(ID,.keep_all = TRUE)
+
+
+
 ####Problem variable
 
 LongY<- LongY%>%
@@ -74,6 +75,7 @@ RTsub=RTdata2[,c("ID","RT.sub1","RT.sub2","RT.sub3","RT.sub4","RT.sub5","RT.sub6
 longRTsub=reshape(RTsub,direction = "long",idvar ="ID", "RT.sub","Problem",varying = list(c("RT.sub1","RT.sub2","RT.sub3","RT.sub4","RT.sub5","RT.sub6")))
 LongYsub=reshape(Ysub,direction = "long",idvar = "ID","Y.sub","Problem", varying = list(c("Y.sub1","Y.sub2","Y.sub3","Y.sub4","Y.sub5","Y.sub6")))
 
+
 ####Creating totalscore variable:
 Ydata1$Y.tot<-rowSums(Ydata1[,c(2:22)])
 RTdata2$RT.tot<-rowSums(RTdata2[,c(2:22)])
@@ -85,25 +87,19 @@ Y.total=Ydata1[,c("ID","Y.tot")]
 ####Smaller dataset with only RT.tot and ID variable
 RT.total=RTdata2[,c("ID","RT.tot")]
 
-##### RTgroup, gender variable
 
-Group1=RTdataG1[,c("ID","Group")]
+###Splitting the group variable, getting variables: gender and language.
 
-#####slicing splitting Group
+Group1=RTdata[,c("ID","Group")]
 
-RTdataG69<- cbind(RTdataG69[-3],                   t(as.data.frame(strsplit(RTdataG69$Group, '_'),                                                row.names=c("Group1", "Group2","Group3"))), row.names=NULL)
+Group2<- cbind(Group1[-3],                   t(as.data.frame(strsplit(Group1$Group, '_'),                                                row.names=c("Group1", "Group2","Group3"))), row.names=NULL)
 
-TOTGRUPPE<-RTdataG69 %>% separate(Group2,into = c("Gender","Language"),sep = -2,convert = TRUE)
+TOTGRUPPE<-Group2 %>% separate(Group2,into = c("Gender","Language"),sep = -2,convert = TRUE)
 
 TOTGRUPPE1=TOTGRUPPE[,c("ID","Gender","Language")]
 
-######New LongRT without group
 
-LongRT1=LongRT[,c("ID","RT","Problem")]
-
-
-####Merging
-
+#####merging
 
 RTYlong=merge(LongRT,LongY)
 
@@ -119,6 +115,6 @@ TOTAL=merge(RTYlong,SUBTOT)
 
 DONEASSIGNMENT2=merge(TOTAL,TOTGRUPPE1)
 
-############EXPORTING
-write.table(DONEASSIGNMENT2,file = "../Data/DONEASSIGNMENT2-exported.txt",row.names = TRUE,sep = ".t",col.names = NA)
+###exporting
 
+write.table(DONEASSIGNMENT2,file = "data/DONEASSIGNMENT2-exported.txt",row.names = TRUE,sep = "\t",col.names = NA)

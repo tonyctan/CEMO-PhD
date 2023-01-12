a846c69f528c4a7a348256d4c425f3d50d2d8211
Tony Tan <tony.c.tan@gmail.com>
2023-01-12 17:37:29 +0100

    Change UV9121 Process data analysis.R file

--- Coursework/UV9121 Process data/Analysis/11_analysis.R
+++ Coursework/UV9121 Process data/Analysis/11_analysis.R
@@ -1,12 +1,14 @@
+        ###################################
+        # Read in financial literacy data #
+        ###################################
+
 # Read in financial literacy data
 setwd("~/uio/pc/Dokumenter/PhD/Coursework/UV9121 Process data/")
 flt <- data.frame(data.table::fread("./Analysis/flt.csv",
     sep = "\t", header = TRUE, nThread = data.table::getDTthreads(),
     na.strings = c("Valid Skip", "Not Applicable", "Invalid", "No Response")
 ))
-
-# Turn reation time from milliseconds to seconds
-flt$FL164_TT <- flt$FL164_TT / 1000
+n0 <- nrow(flt)
 
 # Recode student responses fromt text to numeric
 option <- c(
@@ -24,27 +26,143 @@ for (k in 1:3) {
     }
 }
 flt[, c(4:21)] <- as.numeric(unlist(flt[, c(4:21)]))
+# Out of the 18 items, how many were answeered?
+flt$FL164_ans <- 18 - apply(flt[, c(4:21)], 1, function(x) {
+    sum(is.na(x))
+})
 
-# Generate sum scores of FL164
-flt$FL164_ok_sum <- rowSums(flt[, 4:19]) # First 16 (reasonable) items
-flt$FL164_bs_sum <- rowSums(flt[, 20:21]) # Last 2 ("Are you sure?") items
-flt$FL164_sum <- flt$FL164_ok_sum + flt$FL164_bs_sum
+
+# Turn reation time from milliseconds to seconds
+flt$FL164_TT <- flt$FL164_TT / 1000
+
+# Remove candidates without reacion time
+flt <- flt[which(!is.na(flt$FL164_TT)), ]
+n1 <- nrow(flt)
+# Sort dataset by reaction time
+flt <- flt[order(flt$FL164_TT), ]
+# Inspect the head and tail of reaction time
+head(flt$FL164_TT, 500)
+tail(flt$FL164_TT, 500)
+# Decision: remove candidates with reaction time > 180 seconds (3 minutes)
+flt <- flt[which(flt$FL164_TT < 180), ]
+n2 <- nrow(flt)
+# How many candidates were affected by this 3-minute cutoff?
+n1 - n2 # Answer: 353 candidates
+# Calculate data loss rate
+(n0 - n2) / n0 * 100 # Answer: 3.28%
+
+        ###################################
+        # Study candidates' reaction time #
+        ###################################
+
+# Extract reaction time
+data <- flt$FL164_TT
+# Inspect frequency distribution of reaction time (no need to save this graph)
+plot(density(data),
+    xlab = "Reaction Time (seconds)",
+    ylab = "Density",
+    main = "Distribution of reaction time"
+)
+
+# Create a placeholder table
+n <- length(data)
+temp <- data.frame(matrix(NA, nrow = n - 2, ncol = 2))
+names(temp) <- c("k", "l")
+
+# Compute log-likelihood for each k
+for (k in 1:(n - 2)) {
+    # Partition the data into two parts
+    data_exp <- data[1:k]           # Exponential part
+    data_norm <- data[(k + 1):n]    # Normal part
+    # Compute the log-likelihood
+    temp[k, 1] <- k
+    temp[k, 2] <- - k * log(mean(data_exp)) - k - (n - k) / 2 * log(2 * pi * var(data_norm)) - (n - k) / 2      # Equation (7)
+}
+
+# Locate which row the maximal log-likelihood appears
+maxloglik <- max(temp[, 2])
+line_maxloglik <- temp[which(temp == maxloglik, arr.ind = TRUE)[1], ]
+print(line_maxloglik)
+k_hat <- line_maxloglik[[1]]
+
+# Compute cutoff
+cutoff <- (data[k_hat] + data[k_hat + 1]) / 2
+print(cutoff)
+
+# Visualisation
+#   Left panel: Data distribution
+par(mfrow = c(1, 2))
+hist(data,
+    col = "white", border = "black",
+    xlim=c(min(data), max(data)),
+    xlab="Reaction Time (second)", ylab="Frequency",
+    main="Data Distribution"
+)
+par(new = TRUE)
+plot(density(data),
+    col = "black", lwd = 2,
+    xlim=c(min(data), max(data)),
+    xaxt = "n", yaxt = "n", ann = FALSE
+)
+abline(
+    v = cutoff,
+    col = "blue", lwd = 1, lty = 3
+)
+text(cutoff, 0, round(cutoff, 3),   # Round to 3 decimal places
+    cex = 1, pos = 1,col = "blue")  # character expansion = 100%; pos = below
+
+#   Right panel: Log-likelihood as a function of k
+plot(temp[, 2] ~ temp[, 1],
+    type = "l", col = "red", lwd = 2,
+    xlim = c(0, n), ylim = c(min(temp[, 2]), max(temp[, 2])),
+    xlab = "k (now number)", ylab = "Log-likelihood",
+    main = "Maximum Likelihood"
+)
+abline(
+    v = k_hat,
+    col = "blue", lwd = 1, lty = 3
+)
+text(k_hat, maxloglik, k_hat,
+    cex = 1, pos = 3, col = "blue")
+# # Split data, k_hat
+# data_exp <- data[which(data < cutoff)]
+# data_norm <- data[which(data > cutoff)]
+# plot(density(data_exp),
+#     xlab = "Reaction Time (seconds)", ylab = "Density",
+#     main = "Exponential Part"
+# )
+# abline(v = min(data), lwd = 1, lty = 3, col = "black")
+# abline(v = cutoff, lwd = 1, lty = 3, col = "blue")
+# plot(density(data_norm),
+#     xlab = "Reaction Time (seconds)", ylab = "Density",
+#     main = "Normal Part"
+# )
+# abline(v = cutoff, lwd = 1, lty = 3, col = "blue")
+# abline(v = 180, lwd = 1, lty = 3, col = "black")
+# Reset canvas
+par(mfrow = c(1, 1))
+
+
+# Generate FL164 sum scores
+flt$FL164_contr_sum <- rowSums(flt[, 4:19]) # First 16 (control) items
+flt$FL164_decoy_sum <- rowSums(flt[, 20:21]) # Last 2 (decoy) items
+flt$FL164_sum <- flt$FL164_contr_sum + flt$FL164_decoy_sum
 
 # Inspect frequency distribution of FL164 sum scores
 hist(flt$FL164_sum,
-    xlab = "Sum score of FL164 (PISA 2018 Financial Literacy)",
+    xlab = "Sum Score",
     ylab = "Frequency",
-    main = "frequency Distribution of FL164 Sum Scores"
+    main = "FL164 Sum Scores"
 )
 
 # Generate standard deviation of FL164
-flt$FL164_sd <- apply(flt[, 4:21], 1, sd)
-# Are the sum/mean and standard deviation correlated?
+flt$FL164_sd <- apply(flt[, 4:21], 1, sd, na.rm = TRUE)
+# Inspect response patterns
 pdf("sum.pdf", paper = "a4")
-    plot(flt$FL164_sum, flt$FL164_sd,
-        xlab = "Sum Score ",
+    plot(flt$FL164_ans, flt$FL164_sd,
+        xlab = "Items Answered",
         ylab = "Standard Deviation",
-        main = "FL164's Sum Score vs Standard Deviations"
+        main = "Response Patterns"
     )
     abline(h = 0.35, col = "red")
 dev.off()
@@ -56,10 +174,10 @@ pdf("subj_obj.pdf", paper = "a4r")
 for (i in seq(0.1, 0.5, 0.05)) {
     flt_consistent <- flt[which(flt$FL164_sd < i), ]
     plot(flt_consistent$PV1FLIT ~ flt_consistent$FL164_sum,
-        xlab = "Self-claimed Financial Literacy (FL164 Sum Score)",
-        ylab = "Objectively-measured Financial Literacy (PV1FLIT)",
+        xlab = "FL164 Sum Score",
+        ylab = "Financial Literacy (PV1FLIT)",
         main = paste0(
-            "Subjective vs Objective Financial Literacy (SD = ", i, ")"
+            "Subjective vs Objective Financial Literacy (SD < ", i, ")"
         )
     )
 }
